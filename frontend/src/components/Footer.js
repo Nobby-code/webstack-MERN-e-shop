02a700ff719f251c5f72d93b964ce0494f4876b1
Nobby-code <norbertguda0@gmail.com>
2023-07-19 16:27:55 +0300

    Some changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+          <Col className='text-center py-3'>Copyright &copy; DigiShop</Col>
         </Row>
       </Container>
     </footer>
